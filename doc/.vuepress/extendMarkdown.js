b44a6e101f40a8f99dbde6e1fa554182d19eabfc
songlairui <cnre@live.cn>
2019-06-01 01:42:23 +0800

    fix

--- doc/.vuepress/extendMarkdown.js
+++ doc/.vuepress/extendMarkdown.js
@@ -15,9 +15,9 @@ module.exports = (md) => {
     render: (tokens, idx) => {
       var m = tokens[idx].info.trim().match(/^aim\s+(.*)$/)
       if (tokens[idx].nesting === 1) {
-        return '<div class="lary-aim"><h5>' + m
-          ? md.utils.escapeHtml(m[1])
-          : '' + '</h5>\n'
+        return `<div class="lary-aim"><h5>${
+          m ? md.utils.escapeHtml(m[1]) : ''
+        }</h5>\n`
       } else {
         return '</div>\n'
       }
