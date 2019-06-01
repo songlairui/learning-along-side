37888867063fc15356a30c930c4997157a93a24c
songlairui <cnre@live.cn>
2019-06-01 11:34:20 +0800

    生成目录树,跳过 front matter

--- bin/genCover.js
+++ bin/genCover.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const { genMeta } = require('./genSideBar');
 
-const targetFile = path.resolve(__dirname, '../src/README.md');
+const targetFile = path.resolve(__dirname, '../doc/README.md');
 const meta = genMeta();
 
 const str = `# 文档\n<!-- 自动生成 -->\n\n${meta
--- bin/genSideBar.js
+++ bin/genSideBar.js
@@ -1,84 +1,93 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 
-const targetFolder = path.resolve(__dirname, '../doc');
+const targetFolder = path.resolve(__dirname, '../doc')
 
-const ignore = ['assets', '.vuepress'];
+const ignore = ['assets', '.vuepress']
 
-const AllMeta = [];
+const AllMeta = []
 
 function repeatStr(str, times) {
   return Array.from({ length: times })
     .fill(str)
-    .join('');
+    .join('')
 }
 
 function grabMdFileMeta(file, dir, depth) {
   if (!file.endsWith('README.md')) {
-    depth += 1;
+    depth += 1
   }
   const raw = fs
     .readFileSync(file)
     .toString()
     .split(/\n+/)
     .map((line) => line.trim())
-    .filter((line) => line);
+    .filter((line) => line)
+  let titleLine = 0
+  if (raw[0].startsWith('---')) {
+    let reachMatterEnd = false
+    while (!reachMatterEnd) {
+      titleLine++
+      reachMatterEnd = raw[titleLine].startsWith('---')
+    }
+    titleLine++
+  }
   const title =
-    (raw[0] || raw[1] || '')
+    (raw[titleLine] || '')
       .trim()
       .replace(/^#/, '')
-      .trim() || file;
-  const abPath = path.relative(targetFolder, file).replace(/README.md$/, '');
+      .trim() || file
+  const abPath = path.relative(targetFolder, file).replace(/README.md$/, '')
   return {
     file,
     title,
     abPath,
     parent: `/${path.relative(targetFolder, dir)}`,
     text: `${repeatStr('    ', depth - 1)}- [${title}](/${abPath})`
-  };
+  }
 }
 
 function grabDir(dir = targetFolder, parent = '', depth = 0) {
-  const children = fs.readdirSync(dir);
-  const folderMeta = [];
-  const folders = [];
-  const files = [];
+  const children = fs.readdirSync(dir)
+  const folderMeta = []
+  const folders = []
+  const files = []
   children.forEach((file) => {
-    if (ignore.includes(file)) return;
-    const nextFolder = path.resolve(dir, file);
+    if (ignore.includes(file)) return
+    const nextFolder = path.resolve(dir, file)
     if (file.toLowerCase() === 'readme.md') {
-      files.unshift(nextFolder);
-      return;
+      files.unshift(nextFolder)
+      return
     }
     if (file.endsWith('.md')) {
-      files.push(nextFolder);
-      return;
+      files.push(nextFolder)
+      return
     }
     if (!fs.statSync(nextFolder).isDirectory()) {
-      return;
+      return
     }
-    folders.push(nextFolder);
-  });
+    folders.push(nextFolder)
+  })
   // files first
   files.forEach((file) => {
-    const meta = grabMdFileMeta(file, dir, depth);
-    folderMeta.push(meta);
-  });
-  AllMeta.push(...folderMeta);
+    const meta = grabMdFileMeta(file, dir, depth)
+    folderMeta.push(meta)
+  })
+  AllMeta.push(...folderMeta)
 
   // folder then
   folders.forEach((folder) => {
-    grabDir(folder, dir, depth + 1);
-  });
+    grabDir(folder, dir, depth + 1)
+  })
 }
 
 function genMeta() {
-  AllMeta.length = 0;
-  grabDir();
-  return AllMeta;
+  AllMeta.length = 0
+  grabDir()
+  return AllMeta
 }
 
-module.exports = { genMeta };
+module.exports = { genMeta }
 
 // grabDir();
 
